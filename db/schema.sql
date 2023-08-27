ebd770f929fb47b95ec8a4a054942ab2b4e563b8
sthogue <sthogue16@gmail.com>
2023-08-26 20:36:21 -0500

    fixed schema and seeds syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -5,24 +5,26 @@ USE employee_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department VARCHAR(30) NOT NULL,
-)
+  dept_name VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NOT NULL,
-  department_id INT NOT NULL,
-  FOREIGN KEY (department_id) REFERENCES department(id)
-)
+  department_id INT,
+  FOREIGN KEY (department_id) 
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES roles(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  role_id INT,
+  manager_id INT DEFAULT NULL,
+  FOREIGN KEY (role_id) 
+  REFERENCES roles(id)
   ON DELETE SET NULL
-)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,30 +1,31 @@
-INSERT INTO department (id, name)
+INSERT INTO department (id, dept_name)
 VALUES (1, "Human Resources"),
        (2, "Marketing"),
        (3, "Sales"),
        (4, "IT"),
        (5, "Finance"),
-       (6, "Legal"),
+       (6, "Legal");
 
 
 INSERT INTO roles (id, title, salary, department_id)
-VALUES  (1, "Recruiter", 55000, 1)
-        (2, "Human Resource Manager", 60000, 1)
-        (3, "Marketing Associate", 45000, 2)
-        (4, "Marketing Manager", 60000, 2)
-        (5, "Sales Associate", 50000, 3)
-        (6, "Sales Manager", 60000, 3)
-        (7, "IT Associate", 50000, 4)
-        (8, "IT Manager", 60000, 4)
-        (9, "Financial Analyst", 55000, 5)
-        (10, "Financial Manager", 60000, 5)
-        (11, "Legal Associate", 50000, 6)
-        (12, "Legal Manager", 60000, 6)
-        (13, "Head of Legal Council", 70000, 6)
+VALUES  (1, "Recruiter", 55000, 1),
+        (2, "Human Resource Manager", 60000, 1),
+        (3, "Marketing Associate", 45000, 2),
+        (4, "Marketing Manager", 60000, 2),
+        (5, "Sales Associate", 50000, 3),
+        (6, "Sales Manager", 60000, 3),
+        (7, "IT Associate", 50000, 4),
+        (8, "IT Manager", 60000, 4),
+        (9, "Financial Analyst", 55000, 5),
+        (10, "Financial Manager", 60000, 5),
+        (11, "Legal Associate", 50000, 6),
+        (12, "Legal Manager", 60000, 6),
+        (13, "Head of Legal Council", 70000, 6),
         (14, "Director of IT", 80000, 4),
         (15, "Director of HR", 80000, 1),
         (16, "Director of Sales", 75000, 3),
-        (17, "Accountant", 88000, 5);
+        (17, "Accountant", 88000, 5)
+        (18, "CEO", 1000000, NULL);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (1, "James", "Smith", 1, 15),
@@ -39,8 +40,9 @@ VALUES (1, "James", "Smith", 1, 15),
        (10, "Charles", "Taylor", 10, 17),
        (11, "Christopher", "Anderson", 11, 13),
        (12, "Daniel", "Thomas", 12, 13),
-       (13, "Matthew", "Jackson", 13, NULL),
-       (14, "Anthony", "White", 14, NULL),
-       (15, "Donald", "Harris", 15, NULL),
-       (16, "Mark", "Martin", 16, NULL),
-       (17, "Paul", "Thompson", 17, NULL);
+       (13, "Matthew", "Jackson", 13, 18),
+       (14, "Anthony", "White", 14, 18),
+       (15, "Donald", "Harris", 15, 18),
+       (16, "Mark", "Martin", 16, 18),
+       (17, "Paul", "Thompson", 17, 18)
+       (18, "Steven", "Garcia", 18, NULL);
